bdc5d387824a9b170f1c0a265f0a06d8510e36c0
Domen Kožar <domen@dev.si>
2018-07-10 17:33:49 +0200

    Add cachix, statically linked

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -8811,6 +8811,8 @@ with pkgs;
 
   c-blosc = callPackage ../development/libraries/c-blosc { };
 
+  cachix = haskell.lib.justStaticExecutables haskellPackages.cachix;
+
   capnproto = callPackage ../development/libraries/capnproto { };
 
   ndn-cxx = callPackage ../development/libraries/ndn-cxx { };
